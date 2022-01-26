15f6f6059ee20ca74fcbbd838c037c0f80cd811a
delexii <delia.bute@yahoo.com>
2022-01-26 11:27:15 +0000

    completed project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Modal Window
+
+Project was done as part of The complete JavaScript Course with Udemy.
+
+Useful features learnt:
+
+-[x] Working with classes
+-[x] handling an 'esc' keypress events
+
--- script.js
+++ script.js
@@ -25,3 +25,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 btnClosedModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+        }
+});
